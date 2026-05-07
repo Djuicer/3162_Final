1e4463be53186e37bd81290ff856431a735d7cf2
Djuicer <yannnn0718@gmail.com>
2026-05-07 23:40:48 +1000

    Fix DanceParty script after removing talk buttons

--- scenes/Events/DanceParty/DanceParty.cs
+++ scenes/Events/DanceParty/DanceParty.cs
@@ -2,29 +2,40 @@ using Godot;
 
 public partial class DanceParty : Node2D
 {
+	private enum PartyNpc { None, Classmate, Alumni }
+
+	private Area2D _classmateArea;
+	private Area2D _alumniArea;
+	private Label _interactPromptLabel;
 	private Label _instructionLabel;
 	private Label _statusLabel;
+	private Label _npcNameLabel;
 	private Label _dialogueLabel;
-	private Button _talkClassmateButton;
-	private Button _talkAlumniButton;
 	private Button _finishPartyButton;
 	private VBoxContainer _choiceButtons;
 
 	private bool _classmateDone;
 	private bool _alumniDone;
+	private bool _dialogueOpen;
+	private PartyNpc _nearNpc = PartyNpc.None;
 
 	public override void _Ready()
 	{
+		_classmateArea = GetNode<Area2D>("ClassmateInteractArea");
+		_alumniArea = GetNode<Area2D>("AlumniInteractArea");
+		_interactPromptLabel = GetNode<Label>("UI/InteractPromptLabel");
 		_instructionLabel = GetNode<Label>("UI/Panel/VBox/InstructionLabel");
 		_statusLabel = GetNode<Label>("UI/Panel/VBox/StatusLabel");
+		_npcNameLabel = GetNode<Label>("UI/Panel/VBox/NpcNameLabel");
 		_dialogueLabel = GetNode<Label>("UI/Panel/VBox/DialogueLabel");
-		_talkClassmateButton = GetNode<Button>("UI/Panel/VBox/ActionRow/TalkClassmateButton");
-		_talkAlumniButton = GetNode<Button>("UI/Panel/VBox/ActionRow/TalkAlumniButton");
 		_finishPartyButton = GetNode<Button>("UI/Panel/VBox/ActionRow/FinishPartyButton");
 		_choiceButtons = GetNode<VBoxContainer>("UI/Panel/VBox/Choices");
 
-		_talkClassmateButton.Pressed += StartClassmateDialogue;
-		_talkAlumniButton.Pressed += StartAlumniDialogue;
+		_classmateArea.BodyEntered += _ => OnNpcAreaEntered(PartyNpc.Classmate);
+		_classmateArea.BodyExited += _ => OnNpcAreaExited(PartyNpc.Classmate);
+		_alumniArea.BodyEntered += _ => OnNpcAreaEntered(PartyNpc.Alumni);
+		_alumniArea.BodyExited += _ => OnNpcAreaExited(PartyNpc.Alumni);
+
 		_finishPartyButton.Pressed += FinishParty;
 
 		var state = GlobalVars.Instance;
@@ -33,21 +44,42 @@ public partial class DanceParty : Node2D
 
 		_instructionLabel.Text = "Talk to people and make a good impression.";
 		_statusLabel.Text = "Talk to both people before finishing the party.";
+		_npcNameLabel.Text = "";
 		_dialogueLabel.Text = "";
+		_interactPromptLabel.Visible = false;
 		_finishPartyButton.Disabled = true;
 
 		if (state.PartyEventCompleted)
 		{
 			_dialogueLabel.Text = "The party is already over. You can head back.";
-			_talkClassmateButton.Disabled = true;
-			_talkAlumniButton.Disabled = true;
+			_classmateDone = true;
+			_alumniDone = true;
 			_finishPartyButton.Disabled = false;
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+		_interactPromptLabel.Visible = !_dialogueOpen && _nearNpc != PartyNpc.None;
+		if (_interactPromptLabel.Visible)
+			_interactPromptLabel.Text = "Press E to talk";
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (!_dialogueOpen && @event.IsActionPressed("interact"))
+		{
+			if (_nearNpc == PartyNpc.Classmate)
+				OpenClassmateDialogue();
+			else if (_nearNpc == PartyNpc.Alumni)
+				OpenAlumniDialogue();
 		}
 	}
 
 	private void StartClassmateDialogue()
 	{
-		if (_classmateDone) return;
+		_dialogueOpen = true;
+		_npcNameLabel.Text = "Classmate";
 		ClearChoices();
 		_dialogueLabel.Text = "Hey, I heard you're trying to get back on track before graduation. What have you been working on?";
 		AddChoice("I've been rebuilding my coding skills and working on my portfolio.", () => { var s = GlobalVars.Instance; s.Attributes.IncreaseConfidence(5); s.Attributes.IncreaseNetworking(5); s.PartyGoodChoices += 1; CompleteClassmate("Great answer. You sounded focused and motivated."); });
@@ -57,7 +89,8 @@ public partial class DanceParty : Node2D
 
 	private void StartAlumniDialogue()
 	{
-		if (_alumniDone) return;
+		_dialogueOpen = true;
+		_npcNameLabel.Text = "Alumni / Recruiter";
 		ClearChoices();
 		_dialogueLabel.Text = "I'm helping a few students connect with graduate IT opportunities. What kind of role are you aiming for?";
 		AddChoice("I'm aiming for a graduate developer role and preparing through projects and interview practice.", () => { var s = GlobalVars.Instance; s.Profile.IncreaseCareerReadiness(5); s.Attributes.IncreaseNetworking(10); s.PartyGoodChoices += 1; CompleteAlumni("Strong answer. You made a good professional impression."); });
@@ -68,18 +101,20 @@ public partial class DanceParty : Node2D
 	private void CompleteClassmate(string message)
 	{
 		_classmateDone = true;
-		_talkClassmateButton.Disabled = true;
 		_statusLabel.Text = message;
+		_npcNameLabel.Text = "";
 		ClearChoices();
+		_dialogueOpen = false;
 		UpdateFinishAvailability();
 	}
 
 	private void CompleteAlumni(string message)
 	{
 		_alumniDone = true;
-		_talkAlumniButton.Disabled = true;
 		_statusLabel.Text = message;
+		_npcNameLabel.Text = "";
 		ClearChoices();
+		_dialogueOpen = false;
 		UpdateFinishAvailability();
 	}
 
@@ -118,5 +153,36 @@ public partial class DanceParty : Node2D
 	{
 		foreach (Node child in _choiceButtons.GetChildren())
 			child.QueueFree();
+	}
+
+	private void OpenClassmateDialogue()
+	{
+		if (_classmateDone)
+		{
+			_statusLabel.Text = "You already spoke with them.";
+			return;
+		}
+		StartClassmateDialogue();
+	}
+
+	private void OpenAlumniDialogue()
+	{
+		if (_alumniDone)
+		{
+			_statusLabel.Text = "You already spoke with them.";
+			return;
+		}
+		StartAlumniDialogue();
+	}
+
+	private void OnNpcAreaEntered(PartyNpc npc)
+	{
+		_nearNpc = npc;
+	}
+
+	private void OnNpcAreaExited(PartyNpc npc)
+	{
+		if (_nearNpc == npc)
+			_nearNpc = PartyNpc.None;
 	}
 }
